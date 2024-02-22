0f24e93ba36955f124b9c2a20b0f5a85e5bc649b
CocoisCat <demy176@naver.com>
2024-02-23 08:39:10 +0900

    2-7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+//클래스형 컴포넌트
+export default class App extends Component {
+  
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "poinder",
+    float: "right"
+  }
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
+
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false
+    }
+  ]
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+          
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false}/>
+            
+            <button style={this.btnStyle}>x</button>
+          </div>
+          ))}
+
+        
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
